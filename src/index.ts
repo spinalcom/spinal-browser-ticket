c3a87525aba73b7a80baeafa1eb8d82456702d79
Guillaume <guillaume.meslage@gmail.com>
2021-03-16 11:04:07 +0100

    add: backendInitializer initialized in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 /*
  * Copyright 2020 SpinalCom - www.spinalcom.com
- * 
+ *
  * This file is part of SpinalCore.
- * 
+ *
  * Please read all of the following terms and conditions
  * of the Free Software license Agreement ("Agreement")
  * carefully.
- * 
+ *
  * This Agreement is a legally binding contract between
  * the Licensee (as defined below) and SpinalCom that
  * sets forth the terms and conditions that govern your
  * use of the Program. By installing and/or using the
  * Program, you agree to abide by all the terms and
  * conditions stated or referenced herein.
- * 
+ *
  * If you do not agree to abide by these terms and
  * conditions, do not demonstrate your acceptance and do
  * not install or use the Program.
@@ -30,6 +30,7 @@ import ElementUI from 'element-ui';
 import VTooltip from 'v-tooltip';
 import { i18n } from "./services/i18n";
 import spinalBackend from './services/spinalBackend';
+import BackendInitializer from './services/BackendInitializer';
 import { router } from './router/router';
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -52,6 +53,7 @@ Vue.use(ElementUI);
 Vue.use(VTooltip);
 Vue.use(BootstrapVue)
 spinalBackend.init();
+BackendInitializer.getInstance().init();
 
 new Vue({
   // @ts-ignore
