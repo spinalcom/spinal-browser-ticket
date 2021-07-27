1cb2ee64aaa6a7847d2d2e68d652cbdcc995099d
Guillaume <guillaume.meslage@gmail.com>
2021-07-27 16:54:37 +0200

    add: find one in function

--- src/router/EquipmentApp/backend/EquipmentBack.ts
+++ src/router/EquipmentApp/backend/EquipmentBack.ts
@@ -34,6 +34,47 @@ import { FileSystem } from 'spinal-core-connectorjs_type'
 
 type mapEquipmentItem = Map<string, EquipmentItem[]>
 
+async function findOneInContext(node: SpinalNode<any>, context: SpinalContext<any>,
+  predicate: (node: SpinalNode<any>) => boolean
+): Promise<SpinalNode<any>> {
+  if (typeof predicate !== 'function') {
+    throw new Error('The predicate function must be a function');
+  }
+
+  const seen: Set<SpinalNode<any>> = new Set([node]);
+  let promises: Promise<SpinalNode<any>[]>[] = [];
+  let nextGen: SpinalNode<any>[] = [node];
+  let currentGen: SpinalNode<any>[] = [];
+
+  while (nextGen.length) {
+    currentGen = nextGen;
+    promises = [];
+    nextGen = [];
+
+    for (const object of currentGen) {
+      if (predicate(object)) {
+        return object
+      }
+    }
+    promises = currentGen.map((object) => object.getChildrenInContext(context))
+
+
+    const childrenArrays: SpinalNode<any>[][] = await Promise.all(promises);
+
+    for (const children of childrenArrays) {
+      for (const child of children) {
+        if (!seen.has(child)) {
+          nextGen.push(child);
+          seen.add(child);
+        }
+      }
+    }
+  }
+
+  return undefined;
+}
+
+export { findOneInContext }
 
 export class EquipmentBack {
   initDefer = q.defer();
