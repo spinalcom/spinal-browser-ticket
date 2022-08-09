c471d0565cc4816cd71591b64843bc1f1d06ebb0
Guillaume Meslage <g.meslage@spinalcom.com>
2022-08-09 16:10:05 +0200

    refac: replaced space center with app

--- src/router/router.ts
+++ src/router/router.ts
@@ -30,7 +30,7 @@ Vue.use(VueRouter);
 const AppList = () => import('./AppList/AppList.vue');
 // const TicketCenter = () => import('./TicketCenter/TicketCenter.vue');
 const InsightCenter = () => import('./InsightCenter/InsightCenter.vue');
-const SpaceCenter = () => import('./Space Center/spacecenter.vue');
+const SpaceApp = () => import('./SpaceApp/TheSpaceApp.vue');
 const SpaceCenterGestion = () =>
   import('../compoments/space/component/categoryLstVue.vue');
 //const DataRoom = () => import("./DataRoom/DataRoom.vue")
@@ -46,7 +46,7 @@ export const routes = [
   // { path: '/TicketCenter', name: 'TicketCenter', component: TicketCenter },
   { path: '/DataApp', name: 'DataApp', component: DataApp },
   { path: '/InsightCenter', name: 'InsightCenter', component: InsightCenter },
-  { path: '/Spacecenter', name: 'SpaceCenter', component: SpaceCenter },
+  { path: '/SpaceApp', name: 'SpaceApp', component: SpaceApp },
   { path: '/EquipmentApp', name: 'EquipmentApp', component: EquipmentApp },
   { path: '/TicketApp', name: 'TicketApp', component: TicketApp },
   { path: '/AlarmApp', name: 'AlarmApp', component: AlarmApp },
